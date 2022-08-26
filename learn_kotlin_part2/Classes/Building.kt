ee69b36c1f12fccf4809c470cdd80d7540f83a53
arnslnc <89120645+arnslnc@users.noreply.github.com>
2022-08-26 20:28:32 +0800

    Add files via upload
    Compiled codes

---
+++ learn_kotlin_part2/Classes/Building.kt
@@ -0,0 +1,20 @@
+class Building {
+    val yearBuilt = 2016
+    val height = 400 // Feet
+    val type = "Limestone"
+  }
+  
+  fun main() {
+    // Write your code below 🏙
+    var residentialBuilding = Building()
+    println(residentialBuilding.yearBuilt) 
+    println(residentialBuilding.height)
+    println(residentialBuilding.type)
+  
+    var commercialBuilding = Building()
+    println(residentialBuilding.yearBuilt) 
+    println(residentialBuilding.height)
+    println(residentialBuilding.type)
+  }
+  
+  
